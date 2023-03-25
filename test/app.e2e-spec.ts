af3740d8ffd90a52ddef1d0f22e8d1769218e8f8
DevCorvus <devcorvus.contact@gmail.com>
2023-03-25 09:12:28 -0400

    Initial commit (NestJS setup)

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let httpRequest: request.SuperTest<request.Test>;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    httpRequest = request(app.getHttpServer());
+  });
+
+  it('/health (GET)', () => {
+    return httpRequest.get('/health').expect(200);
+  });
+});
